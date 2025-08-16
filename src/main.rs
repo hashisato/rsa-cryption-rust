a26243bc4f789798441a1554c0e331c18e45e9f6
hashisato <satoki1hashimoto@gmail.com>
2025-08-17 00:41:22 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use rsa_encryption_decryption::rsa_crypto::rsa_console_crypto;
+use rsa_encryption_decryption::rsa_cryption::rsa_console_cryption;
 use rsa_encryption_decryption::rsa_signature::rsa_console_signature;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
-        println!("Usage: cargo run -- <crypto|signature>");
+        println!("Usage: cargo run -- <cryption|signature>");
         return;
     }
 
     match args[1].as_str() {
-        "crypto" => rsa_console_crypto(),
+        "cryption" => rsa_console_cryption(),
         "signature" => rsa_console_signature(),
-        _ => println!("Unknown command. Use 'crypto' or 'signature'."),
+        _ => println!("Unknown command. Use 'cryption' or 'signature'."),
     }
 }
